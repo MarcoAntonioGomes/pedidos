2f885837546a66405259deb7b1c3b28d03a429fd
marco <marco@joinder.me>
2022-09-07 16:36:53 -0300

    Refatorando e criando Classe para Renderizar de MD para HTML. Aplicando DDD para criar as entidades de negocio Capitulo e Ebook. Separação da classe que descobre os titulos.

---
+++ cotuba/src/main/java/cotuba/DescobrirHeading.java
@@ -0,0 +1,27 @@
+package cotuba;
+
+import org.commonmark.node.AbstractVisitor;
+import org.commonmark.node.Heading;
+import org.commonmark.node.Text;
+
+public class DescobrirHeading extends AbstractVisitor {
+
+    private Capitulo capitulo;
+
+    public DescobrirHeading(Capitulo capitulo) {
+        this.capitulo = capitulo;
+    }
+
+    @Override
+    public void visit(Heading heading) {
+        if (heading.getLevel() == 1) {
+            // capítulo
+            String tituloDoCapitulo = ((Text) heading.getFirstChild()).getLiteral();
+            capitulo.setTitulo(tituloDoCapitulo);
+        } else if (heading.getLevel() == 2) {
+            // seção
+        } else if (heading.getLevel() == 3) {
+            // título
+        }
+    }
+}
