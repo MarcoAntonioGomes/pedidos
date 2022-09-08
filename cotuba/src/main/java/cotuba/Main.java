e05670506d65fdd28516818aaa5d569d65ff4b5a
marco <marco@joinder.me>
2022-09-08 08:30:15 -0300

    Refatorando a classe main e criando a classe Cotuba para separar a responsabilidade de orquestração da geração do PDF ou EPUB

--- cotuba/src/main/java/cotuba/Main.java
+++ cotuba/src/main/java/cotuba/Main.java
@@ -2,7 +2,7 @@ package cotuba;
 
 
 import java.nio.file.Path;
-import java.util.List;
+
 
 
 public class Main {
@@ -24,26 +24,8 @@ public class Main {
             arquivoDeSaida = opcoesCLI.getArquivoDeSaida();
             modoVerboso = opcoesCLI.isModoVerboso();
 
-            var renderizador = new RenderizadorMDParaHTML();
-            List<Capitulo> capitulos = renderizador.renderiza(diretorioDosMD);
-
-
-            Ebook ebook = new Ebook();
-            ebook.setFormato(formato);
-            ebook.setArquivoDeSaida(arquivoDeSaida);
-            ebook.setCapitulos(capitulos);
-
-
-            if ("pdf".equals(formato)) {
-                var geradorPDF = new GeradorPDF();
-                geradorPDF.gera(ebook);
-
-            } else if ("epub".equals(formato)) {
-                var geradorEPUB = new GeradorEPUB();
-                geradorEPUB.gera(ebook);
-            } else {
-                throw new IllegalArgumentException("Formato do ebook inválido: " + formato);
-            }
+            var  cotuba = new Cotuba();
+            cotuba.executa(formato, diretorioDosMD, arquivoDeSaida);
 
             System.out.println("Arquivo gerado com sucesso: " + arquivoDeSaida);
 
