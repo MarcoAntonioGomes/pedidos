eca90e7389532f620234a6f8e3c8b5b38eb194f1
marco <marco@joinder.me>
2022-09-13 21:40:32 -0300

    Diminuindo o uso de ifs com o uso de map na interface GeradorEbook

--- cotuba/src/main/java/cotuba/application/GeradorEbook.java
+++ cotuba/src/main/java/cotuba/application/GeradorEbook.java
@@ -4,21 +4,24 @@ import cotuba.domain.Ebook;
 import cotuba.epub.GeradorEPUB;
 import cotuba.pdf.GeradorPDF;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public interface GeradorEbook {
+
+    Map<String, GeradorEbook> GERADORES = new HashMap<String, GeradorEbook>(){{
+       put("pdf", new GeradorPDF());
+       put("epub", new GeradorEPUB());
+    }};
 
     void gera(Ebook ebook);
 
     static GeradorEbook cria(String formato){
 
-        GeradorEbook gerador;
+        GeradorEbook gerador = GERADORES.get(formato);
 
-        if ("pdf".equals(formato)) {
-            gerador = new GeradorPDF();
 
-        } else if ("epub".equals(formato)) {
-            gerador = new GeradorEPUB();
-
-        } else {
+         if(gerador == null) {
             throw new IllegalArgumentException("Formato do ebook inválido: " + formato);
         }
 
